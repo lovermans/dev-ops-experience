cc3731e728bef18b7bb3f4d760e51e36eaa4a526
Hebbie Ilma Adzim <ilma.adzim@gmail.com>
2025-11-01 18:15:03 +0700

    Add Windows BAT Command To Run Ngrok & Soketi

---
+++ ngrok/ngrok.bat
@@ -0,0 +1,3 @@
+echo "Jangan tutup jendela ini. Cukup minimize saja. Terima kasih (TTD Pak Jokowi)."
+cd /d C:\Ngrok
+ngrok start --all
---
+++ soketi/soketi.bat
@@ -0,0 +1,3 @@
+@echo off
+echo "Jangan tutup jendela ini. Cukup minimize saja. Terima kasih (TTD Pak Jokowi)."
+soketi start --config="D:\soketi-config.json"
